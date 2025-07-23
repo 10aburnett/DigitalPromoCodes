40979d7ae3aba19e57d1cea164aa46175e03fb0f
Alex Burnett <alexburnett@MacBook-Pro-2.home>
2025-07-23 20:22:19 +0100

    Fix JavaScript chunk loading by disabling custom webpack splitting

--- next.config.js
+++ next.config.js
@@ -56,67 +56,11 @@ const nextConfig = {
       transform: 'react-beautiful-dnd/{{member}}',
     },
   },
-  // Optimize bundle size with advanced splitting
-  webpack: (config, { dev, isServer }) => {
-    if (!dev && !isServer) {
-      // Advanced chunk splitting for better caching
-      config.optimization.splitChunks = {
-        chunks: 'all',
-        minSize: 20000,
-        minRemainingSize: 0,
-        minChunks: 1,
-        maxAsyncRequests: 30,
-        maxInitialRequests: 30,
-        enforceSizeThreshold: 50000,
-        cacheGroups: {
-          // React and core libraries
-          react: {
-            test: /[\\/]node_modules[\\/](react|react-dom)[\\/]/,
-            name: 'react',
-            priority: 40,
-            chunks: 'all',
-          },
-          // Admin-specific libraries
-          admin: {
-            test: /[\\/]src[\\/]app[\\/]admin[\\/]/,
-            name: 'admin',
-            priority: 30,
-            chunks: 'all',
-          },
-          // Chart libraries (heavy, separate chunk)
-          charts: {
-            test: /[\\/]node_modules[\\/](recharts|react-beautiful-dnd)[\\/]/,
-            name: 'charts',
-            priority: 35,
-            chunks: 'all',
-          },
-          // UI libraries
-          ui: {
-            test: /[\\/]node_modules[\\/](@headlessui|@heroicons|framer-motion)[\\/]/,
-            name: 'ui',
-            priority: 20,
-            chunks: 'all',
-          },
-          // Other vendor libraries
-          vendor: {
-            test: /[\\/]node_modules[\\/]/,
-            name: 'vendors',
-            priority: 10,
-            chunks: 'all',
-          },
-          // Common components
-          common: {
-            name: 'common',
-            minChunks: 2,
-            priority: 5,
-            chunks: 'all',
-            reuseExistingChunk: true,
-          },
-        },
-      };
-    }
-    return config;
-  },
+  // Temporarily disable custom webpack config to fix chunk loading issues
+  // webpack: (config, { dev, isServer }) => {
+  //   // Custom webpack config disabled until chunk naming is resolved
+  //   return config;
+  // },
 };
 
 module.exports = nextConfig;
